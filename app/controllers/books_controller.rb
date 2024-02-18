4ae3d0d64ad81cec1e88bb5ac38d75e3cd536894
EC2 Default User <ec2-user@ip-172-31-21-1.ap-northeast-1.compute.internal>
2024-02-18 06:44:12 +0000

    [Add]create失敗時の遷移先

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -17,13 +17,12 @@ class BooksController < ApplicationController
 
   def create
     @book = Book.new(book_params)
-    @book.user_id = current_user.id
+    @book.user = current_user
    if @book.save
-    flash[:notice] = "Book was successfully created."
     redirect_to book_path(@book.id)
    else
+    @user = current_user
     @books = Book.all
-    flash[:alert] = "Book was unsuccessfully created."
     render :index
    end
   end
