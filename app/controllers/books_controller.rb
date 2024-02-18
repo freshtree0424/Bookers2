ebe63b3243ea8d26984b74905c6260f405444e23
EC2 Default User <ec2-user@ip-172-31-21-1.ap-northeast-1.compute.internal>
2024-02-18 08:03:03 +0000

    [Add]削除機能追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -27,6 +27,21 @@ class BooksController < ApplicationController
    end
   end
 
+  def update
+   @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
   private
 
   def book_params
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   root to: "homes#top"
   get '/homes/about' => 'homes#about', as:'about'
 
-  resources :books, only: [:create, :index, :show, :edit]
+  resources :books, only: [:create, :index, :show, :edit, :update ,:destroy]
   resources :users, only: [:show, :edit, :update, :index]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
