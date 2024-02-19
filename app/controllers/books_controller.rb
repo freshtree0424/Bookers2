874a1882fe1ec28493076624b9bb398390807ddb
EC2 Default User <ec2-user@ip-172-31-21-1.ap-northeast-1.compute.internal>
2024-02-19 08:54:06 +0000

    [Add]アクセス制限

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,6 @@
 class BooksController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
+  
   def index
     @user = current_user
     @books = Book.all
@@ -50,5 +52,12 @@ class BooksController < ApplicationController
 
   def book_params
     params.require(:book).permit(:title, :body)
+  end
+  
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to books_path
+    end
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -37,7 +37,7 @@ class UsersController < ApplicationController
   def is_matching_login_user
     user = User.find(params[:id])
     unless user.id == current_user.id
-      redirect_to post_images_path
+      redirect_to user_path(current_user)
     end
   end
 
