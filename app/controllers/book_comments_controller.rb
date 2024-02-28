c7513987c18c614f9d097b8e022dab92862c57c0
freshtree0424 <srk.mizuki.kh.vat.mdg@gmail.com>
2024-02-28 10:20:26 +0000

    [Add]コメント機能の非同期追加

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,14 +1,15 @@
 class BookCommentsController < ApplicationController
   
   def create
-    book = Book.find(params[:book_id])
-    comment = current_user.book_comments.new(book_comment_params)
-    comment.book_id = book.id
-    comment.save
+    @book = Book.find(params[:book_id])
+    @comment = current_user.book_comments.new(book_comment_params)
+    @comment.book_id = @book.id
+    @comment.save
   end
 
   def destroy
-    BookComment.find(params[:id]).destroy
+    @comment = BookComment.find(params[:id])
+    @comment.destroy
   end
 
   private
