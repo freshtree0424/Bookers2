2aaf3b43f7d844f2badd08c5015c4d456c440eac
freshtree0424 <srk.mizuki.kh.vat.mdg@gmail.com>
2024-02-28 09:38:55 +0000

    [Add]いいね機能非同期追加

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,16 +1,15 @@
 class FavoritesController < ApplicationController
-
+  before_action :authenticate_user!
+  
   def create
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.new(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.new(book_id: @book.id)
     favorite.save
-    redirect_back(fallback_location: root_path)
   end
 
   def destroy
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.find_by(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.find_by(book_id: @book.id)
     favorite.destroy
-    redirect_back(fallback_location: root_path)
   end
 end
