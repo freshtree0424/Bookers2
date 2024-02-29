4249dbed2f7b0256e16ded521ad802b34014219f
freshtree0424 <srk.mizuki.kh.vat.mdg@gmail.com>
2024-02-29 04:22:19 +0000

    [Add]細かいところ編集

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,9 +1,9 @@
 class BookCommentsController < ApplicationController
   
   def create
-    @book = Book.find(params[:book_id])
+    book = Book.find(params[:book_id])
     @comment = current_user.book_comments.new(book_comment_params)
-    @comment.book_id = @book.id
+    @comment.book_id = book.id
     @comment.save
   end
 
