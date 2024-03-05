32ed53ae647151fafa3972176ba6589505646ab4
freshtree0424 <srk.mizuki.kh.vat.mdg@gmail.com>
2024-03-05 00:34:17 +0000

    [Add]いいねに降順追加

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,7 +2,12 @@ class BooksController < ApplicationController
   before_action :is_matching_login_user, only: [:edit, :update]
 
   def index
-    @books = Book.all
+    to  = Time.current.at_end_of_day
+    from  = (to - 6.day).at_beginning_of_day
+    @books = Book.all.sort {|a,b| 
+      b.favorites.where(created_at: from...to).size <=> 
+      a.favorites.where(created_at: from...to).size
+    }
     @book_comment = BookComment.new
     @book = Book.new
   end
--- app/models/book.rb
+++ app/models/book.rb
@@ -3,6 +3,7 @@ class Book < ApplicationRecord
   belongs_to :user
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  has_many :week_favorites, -> { where(created_at: 1.week.ago.beginning_of_day..Time.current.end_of_day) }
 
   validates :title, presence: true
   validates :body, presence: true, length: { maximum: 200 }
